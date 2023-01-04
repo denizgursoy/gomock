7bfff861b084d8661cf3ea4e2c052049828d03f4
denizgursoy <denizgursoy.443@gmail.com>
2023-01-04 21:48:35 +0100

    [#4] add cases

--- logistic/_post.go
+++ logistic/_post.go
@@ -1,14 +1,14 @@
 package logistic
 
-type postService struct {
+type PostService struct {
 	a addressService
 }
 
-func NewPostService(service addressService) postService {
-	return postService{service}
+func NewPostService(service addressService) PostService {
+	return PostService{service}
 }
 
-func (p postService) getPostCode(customerID int64) string {
+func (p PostService) getPostCode(customerID int64) string {
 	address := p.a.GetAddress(customerID, WorkAddress)
 	if address == nil {
 		address = p.a.GetAddress(customerID, HomeAddress)
--- logistic/post_test.go
+++ logistic/post_test.go
@@ -7,13 +7,26 @@ import (
 )
 
 func TestPostService_GetPostCode(t *testing.T) {
-	t.Run("should return work address if it exists", func(t *testing.T) {
+	t.Run("should return work address if it is not nil", func(t *testing.T) {
+		//service := NewPostService(PostService{})
+		//code := service.GetPostCode(1)
+		//
+		//require.Equal(t, "2544TT", code)
+	})
+
+	t.Run("should return work address if it is not nil", func(t *testing.T) {
+		service := NewPostService(mockAddressService{})
+		code := service.GetPostCode(1)
+
+		require.Equal(t, "2544TT", code)
+	})
+
+	t.Run("should return work address if it is not nil", func(t *testing.T) {
 		controller := gomock.NewController(t)
 		defer controller.Finish()
 
 		mockLocator := NewMockLocator(controller)
-
-		mockLocator.EXPECT().GetAddress(1, WorkAddress).Return(&Address{"2544TT"})
+		mockLocator.EXPECT().GetAddress(int64(1), WorkAddress).Return(&Address{"2544TT"})
 
 		service := NewPostService(mockLocator)
 
@@ -22,17 +35,53 @@ func TestPostService_GetPostCode(t *testing.T) {
 		require.Equal(t, "2544TT", code)
 	})
 
-	t.Run("should return work address if it exists", func(t *testing.T) {
-		service := NewPostService(mockAddressService{})
-		code := service.GetPostCode(1)
+	t.Run("should return home address if work address is nil", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
 
-		require.Equal(t, "2544TT", code)
+		mockLocator := NewMockLocator(controller)
+		mockLocator.EXPECT().GetAddress(int64(2), WorkAddress).Return(nil)
+		mockLocator.EXPECT().GetAddress(int64(2), HomeAddress).Return(&Address{"1111TK"})
+
+		service := NewPostService(mockLocator)
+
+		code := service.GetPostCode(2)
+
+		require.Equal(t, "1111TK", code)
 	})
 
-	t.Run("should return work address if it exists", func(t *testing.T) {
-		//service := NewPostService(PostService{})
-		//code := service.GetPostCode(1)
-		//
-		//require.Equal(t, "2544TT", code)
+	t.Run("arguments matcher", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		mockLocator := NewMockLocator(controller)
+
+		// Eq makes sure that first argument is equal to value
+		mockLocator.EXPECT().GetAddress(gomock.Eq(int64(2)), WorkAddress)
+		// Not makes sure that first argument is not equal to value
+		mockLocator.EXPECT().GetAddress(gomock.Not(int64(1)), WorkAddress)
+		// Any is like a placeholder for the argument, it does not check value
+		mockLocator.EXPECT().GetAddress(gomock.Any(), HomeAddress)
+		// Nil makes sure that first argument is nil
+		mockLocator.EXPECT().GetAddress(gomock.Nil(), HomeAddress)
+		// Len method checks the length of the first argument is equal to value.
+		// It can be useful for checking size of arrays and maps arguments
+		mockLocator.EXPECT().GetAddress(gomock.Len(1), HomeAddress)
+		// All makes sure that argument matched by all matchers provided
+		mockLocator.EXPECT().GetAddress(gomock.All(gomock.Not(int64(1)), gomock.Not(int64(2))), HomeAddress)
+		// AssignableToTypeOf checks if the types of the arguments matches
+		mockLocator.EXPECT().GetAddress(gomock.AssignableToTypeOf(int64(1)), HomeAddress)
+
+		mockLocator.EXPECT().GetAddress(gomock.Any(), gomock.Any()).
+			DoAndReturn(func(customerID int64, addressType string) *Address {
+				if customerID == 1 {
+					return &Address{"2544TT"}
+				} else if customerID == 2 {
+					return &Address{"1111TK"}
+				} else if customerID == 3 {
+					return &Address{"6789GH"}
+				}
+				return nil
+			})
 	})
 }
